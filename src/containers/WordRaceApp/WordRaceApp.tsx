6fef811c127337c70a76c70675e991b54215f33d
Srijan Srivastava <srijan.srivastava35@gmail.com>
2022-01-26 15:13:02 +0530

    💅beautified, added comments and made leveling rewarding

--- src/containers/WordRaceApp/WordRaceApp.tsx
+++ src/containers/WordRaceApp/WordRaceApp.tsx
@@ -39,11 +39,13 @@ import {
   setIntervalTimer,
   shouldStopTimer,
 } from '../../features/timer/state/timerSlice';
-
+/** Initialize sound effects */
 const warningSound = require('../../assets/sounds/warning.mp3');
 const failedSound = require('../../assets/sounds/failed.mp3');
+const successSound = require('../../assets/sounds/countdown.mp3');
 
 function WordRaceApp() {
+  /** Initialize selectors */
   const score = useAppSelector(selectScore);
   const baseScore = useAppSelector(selectBaseScore);
   const status = useAppSelector(selectStatus);
@@ -62,10 +64,14 @@ function WordRaceApp() {
     volume: 0.75,
     interrupt: true,
   });
-
+  const [playSuccess] = useSound(successSound, {
+    volume: 0.75,
+  });
   useEffect(() => {
+    /** If the status is over, reset scores, play the sound effect */
     switch (status) {
       case 'OVER':
+      case 'FINISHED':
         dispatch(setKey(''));
         dispatch(setKeyCount(0));
         dispatch(setCharacter(0));
@@ -74,23 +80,34 @@ function WordRaceApp() {
         dispatch(shouldStopTimer(true));
         dispatch(setMaxWordStackLength(0));
         pause();
-        playFailed();
+        if (status === 'OVER') {
+          playFailed();
+        } else {
+          playSuccess();
+        }
         break;
       default:
         break;
     }
   }, [status]);
 
+  /** Recieve the status of the word typed from Keyboard.tsx */
   useEffect(() => {
     if (triggerCorrect) {
+      /** Set the next word */
       const nextWordCount = wordIndexValue + 1;
       dispatch(setWordIndex(nextWordCount));
       dispatch(setKey(''));
       dispatch(changeWord(nextWordCount));
       dispatch(setKeyCount(0));
       dispatch(setCharacter(0));
-      const scoreToIncrement = (baseScore - wordStackLength) * wordIndexValue;
+      /** The score is calculated on the basis of a base score, decremented by word stack length,
+       * multiplied by the the number of words typed added with the current level */
+      const scoreToIncrement =
+        (baseScore - wordStackLength) * (wordIndexValue + currentLevel);
       dispatch(incrementScore(scoreToIncrement));
+
+      /** Handle leveling upto level 4 */
       switch (wordIndexValue) {
         case 5:
           dispatch(setLevel(2));
@@ -100,18 +117,25 @@ function WordRaceApp() {
           dispatch(setLevel(3));
           dispatch(setIntervalTimer(3));
           break;
-        case 11:
+        case 15:
+          dispatch(setLevel(4));
+          dispatch(setIntervalTimer(2));
+          break;
+        case 20:
           dispatch(recieveAppStatus('FINISHED'));
           break;
         default:
           break;
       }
     }
+
+    /** Reset status of isCorrect to false */
     return () => {
       dispatch(recieveCorrect(false));
     };
   }, [triggerCorrect]);
 
+  /** Play warning sound to alert user that stack is almost done, hurry! */
   useEffect(() => {
     if (maxWordStackLength === 3 || maxWordStackLength === 2) {
       playWarning();
@@ -122,6 +146,10 @@ function WordRaceApp() {
     <div className="flex-column">
       <main className={cx(styles.main)}>
         <div className={cx(styles.header, 'frosted-glass', 'flex-row')}>
+          <h1>
+            Word Race!
+            <img src={logo} className={cx(styles.logo)} alt="logo" />
+          </h1>
           {maxWordStackLength < 4 ? (
             <div className={cx(styles.wordsLeft, 'flex-column')}>
               <p>{maxWordStackLength} left</p>
@@ -131,10 +159,6 @@ function WordRaceApp() {
             ''
           )}
           <Timer />
-          <h1>
-            Word Race!
-            <img src={logo} className={cx(styles.logo)} alt="logo" />
-          </h1>
           <div className="flex-column">
             <p>{score}</p>
             <p className="bold">SCORE</p>
